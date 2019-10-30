e586b81a0811f2444326341bd0e3a0e6e747bb29
John Baylor <john.baylor@hoteltonight.com>
2019-10-30 14:45:23 -0700

    restore assume_role.

--- dev/provider.tf
+++ dev/provider.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
   allowed_account_ids = ["909687094321"]
 
-  #assume_role {
-  #  role_arn = "arn:aws:iam::909687094321:role/ht_test_in_personal_account" # for JohnB personal account for testing HT TFE
-  #}
+  assume_role {
+    role_arn = "arn:aws:iam::909687094321:role/ht_test_in_personal_account" # for JohnB personal account for testing HT TFE
+  }
 }
 
